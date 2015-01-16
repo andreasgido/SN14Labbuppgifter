4044cc8790ab67fb7510c0b1baa0fec49e364048
Andreas Gidö <andreas.gido@gmail.com>
2015-01-16 15:19:54 +0100

    Lagt en lösning.

---
+++ Labb4 Gissa Talet/Labb4NivaB/Laboration4.B/Laboration4.B/SecretNumber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration4.B
+{
+    public class SecretNumber
+    {
+        // Fältvariablar.
+        private int[] _guessedNumbers;
+        private int _number;
+
+        public const int MaxNumberOfGuesses = 7;
+
+        // Egenskaper (properties).
+        private bool CanMakeGuesses
+        {
+            get { }
+            private set { }
+        }
+
+        public int Count 
+        {
+            get { return; }
+            private set 
+            { 
+
+            }
+        }
+
+        public int GuesseLeft 
+        {
+            get { return MaxNumberOfGuesses; }
+            
+        }
+
+        // Kontruktor.
+        public SecretNumber()
+        {
+            _guessedNumbers = new int[1];
+            Initialize();
+
+        }
+
+        // Metoden Initialize. Initierar fält och egenskaper.
+        public void Initialize()
+        {
+            
+            Random randomNumber = new Random();
+            _number = randomNumber.Next(1, 101);           
+
+            CanMakeGuesses = true;
+            Count = 0;
+        }
+    
+
+    
+
+        // Metoden MakeGuess.
+    }
+}
