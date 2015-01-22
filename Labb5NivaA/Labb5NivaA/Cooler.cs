ef5bc2872fae3e60995d196a0f01c592394ba0da
Andreas Gidö <andreas.gido@gmail.com>
2015-01-22 18:57:13 +0100

    Lösning till Labb5 Nivå A

---
+++ Labb5NivaA/Labb5NivaA/Cooler.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb5NivaA
+{
+    public class Cooler
+    {
+        // Fält.
+        private decimal _insideTemperature;
+        private decimal _targetTemperature;
+        private const decimal OutsideTemperature = 23.7m;
+
+        // Egenskaper.
+        public bool IsOpen { get; set; }
+
+        public decimal InsideTemperature 
+        { 
+            get { return _insideTemperature; }
+            set
+            {
+                if ( value < 0 || value > 45)
+                {
+                    string errorInsideTemp = "Innertemperaturen är inte i intervallet 0 - 45°C";
+                    throw new ArgumentException(errorInsideTemp);
+                }
+                _insideTemperature = value;               
+            }
+        }
+
+        public bool IsOn { get; set; }
+
+        public decimal TargetTemperature 
+        { 
+            get { return _targetTemperature; }
+            set 
+            {
+                if ( value < 0 || value > 20 )
+                {
+                    string errorTargetTemp = "Måltemperaturen är inte i intervallet 0 - 20°C.";
+                    throw new ArgumentException(errorTargetTemp);               
+                }
+                _targetTemperature = value;
+            }
+        }
+        
+        // Konstruktorer       
+        public Cooler()
+            : this (0m, 0m)                           
+        {
+
+        }
+
+        public Cooler(decimal insideTemperature, decimal targetTemperature)
+            :this(insideTemperature, targetTemperature, false, false)
+        {
+
+        }
+
+        public Cooler(decimal insideTemperature, decimal targetTemperature, bool isOn, bool isOpen)
+        {
+            InsideTemperature = insideTemperature;
+            TargetTemperature = targetTemperature;
+            IsOn = isOn;
+            IsOpen = isOpen;                              
+        }
+
+        // Metoden Tick(). 
+        public void Tick()
+        {
+            decimal change = 0.0m;
+
+            if (IsOn == true && IsOpen == false)
+            {
+                change = -0.2m;
+            }
+            else if (IsOn == true && IsOpen == true)
+            {
+                change += 0.2m;
+            }
+            else if (IsOn == false && IsOpen == false)
+            {
+                change += 0.1m;
+            }
+            else if (IsOn == false && IsOpen == true)
+            {
+                change += 0.5m;
+            }
+
+            // Kollar om temperaruten inne i kylskåpet går under börvärdet(TargetTemperature)
+            if (InsideTemperature + change < TargetTemperature)
+            {
+                InsideTemperature = TargetTemperature;
+            }
+            else
+            {
+                InsideTemperature += change;
+            }
+
+            // Kollar om temperaturen inne i kylskåpet går över rumstemperaturen(OutsideTemperature)
+            if (InsideTemperature + change > OutsideTemperature)
+            {
+                InsideTemperature = OutsideTemperature;
+            }
+
+        }
+
+        // Metoden ToString. Sätter ihop en sträng till metodanropet från Program-klassen där utskrift sker.
+        public override string ToString()
+        {
+            string on = (IsOn == true) ? "[PÅ]" : "[AV]";
+            string open = (IsOpen == true) ? "Öppet" : "Stängt";
+            return String.Format("{0} : {1:f1}°C : ({2:f1}°C) - {3}", on, InsideTemperature, TargetTemperature, open);            
+        }
+
+    }
+}
---
+++ Labb5NivaA/Labb5NivaA/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb5NivaA
+{
+    class Program
+    {
+        // Fält
+        private const string HorisontalLine ="═";
+
+        static void Main(string[] args)
+        {
+            // Test 1.
+            Cooler cooler1 = new Cooler();
+            ViewTestHeader("Test1.\nTest av standardkonstruktorn.\n");
+            Console.WriteLine(cooler1.ToString());
+            //Console.WriteLine();
+           
+            // Test 2.
+            Cooler cooler2 = new Cooler(24.5m , 4);
+            ViewTestHeader("Test2.\nTest av konstruktorn med 2 parametrar, (24,5 och 4).\n");
+            Console.WriteLine(cooler2.ToString());
+            //Console.WriteLine();
+
+            // Test3.
+            Cooler cooler3 = new Cooler(19.5m, 4, true, false);
+            ViewTestHeader("Test3.\nTest av konstruktern med 4 parametrar, (19,5 , 4 , True och False).\n");
+            Console.WriteLine(cooler3.ToString());
+            //Console.WriteLine();
+
+            // Test4.
+            Cooler cooler4 = new Cooler(5.3m, 4, true, false);
+            ViewTestHeader("Test4.\nTest av kylning med metoden Tick().\n");
+            Run(cooler4, 10);
+
+            // Test5.
+            Cooler cooler5 = new Cooler(5.3m, 4, false, false);
+            ViewTestHeader("Test5.\nTest av kylning med metoden Tick(), vara avslaget och med stängd dörr.\n");
+            Run(cooler5, 10);
+
+            // Test6.
+            Cooler cooler6 = new Cooler(10.2m, 4, true, true);
+            ViewTestHeader("Test6.\nTest av påslaget kylskåp och öppen dörr med metoden Tick().\n");
+            Run(cooler6, 10);
+
+            // Test7:
+            Cooler cooler7 = new Cooler(19.7m, 4, false, true);
+            ViewTestHeader("Test7.\nTest av avslaget kylskåp och öppen dörr med metoden Tick().\n");
+            Run(cooler7, 10);
+
+            // Test8. Gör fler try-catch satser
+            ViewTestHeader("Test8.\nTest av egenskaperna så att undantag kastas då innertemperatur och måltemperatur tilldelas felaktiga värden\n");
+            try
+            {               
+                Cooler cooler8 = new Cooler();
+                cooler8.InsideTemperature = 46.0m;      // Tilldela egenskapen InsideTemperature ett felaktigt värde.
+                Run(cooler8, 10);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                ViewErrorMessage(ex.Message);              
+            }
+
+            try
+            {
+                Cooler cooler8 = new Cooler();               
+                cooler8.TargetTemperature = 21;     // Tilldela egenskapen InsideTemperature ett felaktigt värde.
+                Run(cooler8, 10);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                ViewErrorMessage(ex.Message);
+            }
+
+            // Test9. Gör fler try-catch satser
+            ViewTestHeader("Test9.\nTest av konstruktorer så att undantag kastas då innertemperatur och måltemperatur tilldelas felaktiga värden\n");
+            try
+            {
+                Cooler cooler9 = new Cooler(50.0m, 8, true, true);
+                ViewTestHeader("Test9.\nTest av konstruktorer så att undantag kastas då innertemperatur och måltemperatur tilldelas felaktiga värden");
+                Run(cooler9, 10);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine();
+                ViewErrorMessage(ex.Message);
+            }
+
+            try
+            {
+                Cooler cooler9 = new Cooler(7.0m, 30, true, true);
+                ViewTestHeader("Test9.\nTest av konstruktorer så att undantag kastas då innertemperatur och måltemperatur tilldelas felaktiga värden");
+                Run(cooler9, 10);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                ViewErrorMessage(ex.Message);
+            }
+        }
+
+        // Testmetod.
+        public static void Run(Cooler cooler, int minutes)
+        {
+            Console.WriteLine(cooler.ToString());
+
+            for (int countMinutes = 0; countMinutes < 10; countMinutes++)
+            {               
+                cooler.Tick();
+                Console.WriteLine(cooler.ToString());
+            }
+        }
+
+        // Visar felmeddelande.
+        public static void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        // Visar en Test-Header.
+        public static void ViewTestHeader(string header)
+        {
+            //Console.WriteLine();
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write(HorisontalLine);
+            }
+            Console.WriteLine();
+            Console.WriteLine(header);
+        }
+    }
+}
